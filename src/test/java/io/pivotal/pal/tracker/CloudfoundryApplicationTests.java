72bf81be0266e998a5a4cfbfb5fc68e493967e5d
Ubuntu <ubuntu@ip-172-31-30-240.ap-southeast-1.compute.internal>
2020-12-08 08:50:32 +0000

    pal-tracker

---
+++ src/test/java/io/pivotal/pal/tracker/CloudfoundryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pivotal.pal.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudfoundryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
