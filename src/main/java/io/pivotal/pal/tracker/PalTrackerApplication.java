5191a24fbbad9341367f1092738ead01a9f5e92e
Ubuntu <ubuntu@ip-172-31-30-240.ap-southeast-1.compute.internal>
2020-12-09 08:57:33 +0000

    dbmigration

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+/*Author - Pawan Patil , Hiren Tamhane , Prasahant agrawal*/
 @SpringBootApplication
 public class PalTrackerApplication {
 
